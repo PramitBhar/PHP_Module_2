cf34f080de436b24f601cb4a2d3a9b6370207fd2
PramitBhar <pramit.bhar@innoraft.com>
2024-03-16 00:10:52 +0530

    FT2-37-7: Replace assignment questions with their solution using path.

--- index.php
+++ index.php
@@ -1,19 +1,21 @@
 <?php
-
 session_start();
+
+$_SESSION["username"] = "pramibhar";
+$_SESSION["password"] = "4567";
 if($_SERVER["REQUEST_METHOD"]=="POST") {
   $login_username = $_POST["username"];
   $login_password = $_POST["password"];
-  $_SESSION["username"] = $login_username;
-  $_SESSION["password"] = $login_password;
-  if(isset($_GET["q"])) {
-
-    $p = $_GET["q"];
-    header('location:questionlink.php?q='.$p);
-  }
-  else {
-    $p = 1;
-    header('location:questionlink.php?q='.$p);
+  if($_SESSION["username"] == $login_username && $_SESSION["password"] == $login_password) {
+     $_SESSION["flag"] = 1;
+    if(isset($_GET["q"])) {
+      $p = $_GET["q"];
+      header('location:questionlink.php?q=' . $p);
+    }
+    else {
+      $p = 1;
+      header('location:questionlink.php?q=' . $p);
+    }
   }
 }
 ?>
@@ -23,14 +25,16 @@ if($_SERVER["REQUEST_METHOD"]=="POST") {
   <title>Login Page</title>
 </head>
 <body>
-  <h2>Login</h2>
-  <?php echo $message?>
-  <form method="post" action=''>
-    Username: <input type="text" name="username"><br><br>
-    Password: <input type="password" name="password"><br><br>
-    <a href="./index2.php">Don't have an account?</a>
-    <input type="submit" value="Login">
-  </form>
+  <div>
+    <h2>Login</h2>
+    <form method="post" action='<?php echo
+        htmlspecialchars($_SERVER["PHP_SELF"]); ?>'>
+      Username: <input type="text" name="username"><br><br>
+      Password: <input type="password" name="password"><br><br>
+      <a href="./index2.php">Don't have an account?</a>
+      <input type="submit" value="Login">
+    </form>
+  </div>
 </body>
 </html>
 
